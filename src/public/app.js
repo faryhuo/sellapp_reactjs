6dec531100ee9a1ddd76cabe2a543e2bc8a565c8
fary <910146725@qq.com>
2018-11-23 15:19:11 +0800

    Fary: remove some component

--- src/public/app.js
+++ src/public/app.js
@@ -5,11 +5,7 @@ import ReactDOM from 'react-dom';
 
 import HomePage from 'page/HomePage/HomePage.js';
 import SellerDetail from 'page/SellerDetail/SellerDetail.js';
-import { TransitionGroup, CSSTransition } from "react-transition-group";
-
 import '@/common/stylus/index.styl';
-import './app.css';
-import 'animate.css';
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 class App extends React.Component {
@@ -17,29 +13,14 @@ class App extends React.Component {
         return (
             <div id="App_Conponent" className="Application">
                 <Router>
-                    <Route
-                        render={({ location }) => (
-                            <div >
-                                <Route exact path="/" render={() => <Redirect to="/SellerList/home" />}/>
-
-                                <div >
-                                    {/* <TransitionGroup>
-                                        <CSSTransition
-                                            key={location.key}
-                                            classNames="fade"
-                                            timeout={300}
-                                        > */}
-                                    <Switch location={location}>
-                                        <Route path="/SellerList" component={HomePage}></Route>
-                                        <Route path="/Detail/:id" component={SellerDetail}></Route>
-                                        <Route render={() => <div>Not Found</div>} />
-                                    </Switch>
-                                    {/* </CSSTransition>
-                                    </TransitionGroup> */}
-                                </div>
-                            </div>
-                        )}
-                    ></Route>
+                    <div>
+                        <Route exact path="/" render={() => <Redirect to="/SellerList/home" />} />
+                        <Switch>
+                            <Route path="/SellerList" component={HomePage}></Route>
+                            <Route path="/Detail/:id" component={SellerDetail}></Route>
+                            <Route render={() => <div>Not Found</div>} />
+                        </Switch>F
+                    </div>
                 </Router>
             </div>
         );
